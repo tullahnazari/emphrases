e0750e9b7f46f20f52ce2ee8a257822d8d61f37e
tullahnazari <tullah.nazari@gmail.com>
2019-08-25 11:26:34 -0500

    added first 2 GET calls

--- src/Application.kt
+++ src/Application.kt
@@ -3,11 +3,21 @@ package com.tullahnazari.emphrases
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+        get("/") {
+            call.respondText("Hello, Kids")
+        }
+        get("/adults") {
+            call.respondText("Hello, Adults")
+        }
+    }
 }
 
